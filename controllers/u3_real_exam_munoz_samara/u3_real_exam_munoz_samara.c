b6b960ea18a3b5f3c8070fd01263cb9743792443
Samara <al1609086@upy.edu.mx>
2019-07-17 16:45:04 -0500

    Tank Assembled with Rotational and Position Sensors

--- controllers/u3_real_exam_munoz_samara/u3_real_exam_munoz_samara.c
+++ controllers/u3_real_exam_munoz_samara/u3_real_exam_munoz_samara.c
@@ -11,6 +11,15 @@
  * <webots/differential_wheels.h>, etc.
  */
 #include <webots/robot.h>
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/position_sensor.h>
+#include <webots/distance_sensor.h>
+#include <webots/keyboard.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /*
  * You may want to add macros here.
@@ -27,31 +36,45 @@ int main(int argc, char **argv)
   /* necessary to initialize webots stuff */
   wb_robot_init();
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
+  WbDeviceTag wheel_1 = wb_robot_get_device("wheel1");
+  WbDeviceTag wheel_2 = wb_robot_get_device("wheel2");
+  WbDeviceTag wheel_3 = wb_robot_get_device("wheel3");
+  WbDeviceTag distanceMotor = wb_robot_get_device("ds_motor");
+  WbDeviceTag gunMotor = wb_robot_get_device("gun_motor");
+   
+  WbDeviceTag pos1 = wb_robot_get_device("ps_1");
+  WbDeviceTag pos2 = wb_robot_get_device("ps_2");
+  WbDeviceTag pos3 = wb_robot_get_device("ps_3");
+  WbDeviceTag distancePos = wb_robot_get_device("ds_position");
+  WbDeviceTag gunPos = wb_robot_get_device("gun_position");
 
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
+   
+   
+   wb_position_sensor_enable(pos1,TIME_STEP);
+   wb_position_sensor_enable(pos2,TIME_STEP);
+   wb_position_sensor_enable(pos3,TIME_STEP);
+   wb_position_sensor_enable(distancePos,TIME_STEP);
+   wb_position_sensor_enable(gunPos,TIME_STEP);
+   
+   wb_motor_set_position(wheel_1,INFINITY);
+   wb_motor_set_position(wheel_2,INFINITY);
+   wb_motor_set_position(wheel_3,INFINITY);
+   wb_motor_set_position(distanceMotor,INFINITY);
+   wb_motor_set_position(gunMotor,INFINITY);
+
+   wb_motor_set_velocity(wheel_1,-3);
+   wb_motor_set_velocity(wheel_2,3);
+   wb_motor_set_velocity(wheel_3,0); 
+   wb_motor_set_velocity(distanceMotor,3); 
+   wb_motor_set_velocity(gunMotor,-3);
+
+
   while (wb_robot_step(TIME_STEP) != -1) {
 
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
 
-    /* Process sensor data here */
 
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
+
+
   };
 
   /* Enter your cleanup code here */
